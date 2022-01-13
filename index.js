549d97ecdeb6284f79c16fa10c871bc7c4d02bb0
Kimberly Hayes <kshayes78@gmail.com>
2022-01-13 11:31:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function superbowlWin(records){
+    // records = [{},{},{},{}]
+  
+    const winRecord = records.find((record) => record.result === "W")
+    
+    // => {result: "W", year: "1995"}
+    // return winRecord?.year
+    
+    if (winRecord === undefined){
+        return undefined
+    }else{
+        return winRecord.year
+    }
+    
+
+}
+
+
+
