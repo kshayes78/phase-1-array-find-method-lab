2f35e27823de40702d6a1d8fe29f4e7cd03236fa
Kimberly Hayes <kshayes78@gmail.com>
2022-01-13 11:30:52 -0500

    commit tree a42e6e7aed3f4c21d09b3cce205fe6c9d82dd5ce

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function superbowlWin(records){
+    // records = [{},{},{},{}]
+  
+    const winRecord = records.find((record) => record.result === "W")
+    
+    // => {result: "W", year: "1995"}
+    // return winRecord?.year
+    
+    if (winRecord === undefined){
+        return undefined
+    }else{
+        return winRecord.year
+    }
+    
+//  if (result.find = "W")
+//  return record.result
+
+   //return (array === "W");
+}
+
+
+// const winRecord = records.find(findWinFromObj)
+
+// function findWinFromObj(fish){
+//     fish.result === "W"
+// }
